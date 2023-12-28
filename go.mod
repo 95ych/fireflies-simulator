66250e9e13a524841f61c6ee0bf543b91b0c9fea
C Y K Sagar <cyksagar@protonmail.com>
2023-12-29 00:58:41 +0530

    add slider

--- go.mod
+++ go.mod
@@ -2,15 +2,20 @@ module fireflies
 
 go 1.20
 
-require github.com/hajimehoshi/ebiten/v2 v2.4.16
+require (
+	github.com/ebitenui/ebitenui v0.5.5
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/hajimehoshi/ebiten/v2 v2.5.0
+	golang.org/x/image v0.14.0
+)
 
 require (
-	github.com/ebitengine/purego v0.0.0-20220905075623-aeed57cda744 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220806181222-55e207c401ad // indirect
-	github.com/hajimehoshi/file2byteslice v0.0.0-20210813153925-5340248a8f41 // indirect
-	github.com/jezek/xgb v1.0.1 // indirect
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.1.0 // indirect
-	golang.org/x/mobile v0.0.0-20220722155234-aaac322e2105 // indirect
-	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+	github.com/ebitengine/purego v0.4.0 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/jezek/xgb v1.1.0 // indirect
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
+	golang.org/x/exp/shiny v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 	"math"
 	"math/rand"
 	"time"
 
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
+	"github.com/golang/freetype/truetype"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 const (
@@ -103,12 +109,87 @@ func (fly *Firefly) Draw(screen *ebiten.Image) {
 
 type Game struct {
 	flies [flyCount]Firefly
+	ui    *ebitenui.UI
 }
 
 func NewGame() *Game {
 	g := &Game{}
 	for i := 0; i < flyCount; i++ {
-		g.flies[i].Init()
+		go g.flies[i].Init()
+	}
+	// This loads a font and creates a font face.
+	ttfFont, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		log.Fatal("Error Parsing Font", err)
+	}
+	fontFace := truetype.NewFace(ttfFont, &truetype.Options{
+		Size: 32,
+	})
+
+	// This creates a text widget that says "Hello World!"
+	helloWorldLabel := widget.NewText(
+		widget.TextOpts.Text("Slider", fontFace, color.White),
+	)
+
+	// construct a new container that serves as the root of the UI hierarchy
+	rootContainer := widget.NewContainer(
+		// the container will use a plain color as its background
+		// widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{0x13, 0x1a, 0x22, 0xff})),
+
+		// the container will use an anchor layout to layout its single child widget
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+
+	// construct a slider
+	slider := widget.NewSlider(
+		// Set the slider orientation - n/s vs e/w
+		widget.SliderOpts.Direction(widget.DirectionHorizontal),
+		// Set the minimum and maximum value for the slider
+		widget.SliderOpts.MinMax(0, 10),
+
+		widget.SliderOpts.WidgetOpts(
+			// Set the Widget to layout in the center on the screen
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				StretchHorizontal: false,
+				StretchVertical:   false,
+			}),
+			// Set the widget's dimensions
+			widget.WidgetOpts.MinSize(200, 6),
+		),
+		widget.SliderOpts.Images(
+			// Set the track images
+			&widget.SliderTrackImage{
+				Idle:  image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+				Hover: image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+			},
+			// Set the handle images
+			&widget.ButtonImage{
+				Idle:    image.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
+				Hover:   image.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
+				Pressed: image.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
+			},
+		),
+		// Set the size of the handle
+		widget.SliderOpts.FixedHandleSize(6),
+		// Set the offset to display the track
+		widget.SliderOpts.TrackOffset(0),
+		// Set the size to move the handle
+		widget.SliderOpts.PageSizeFunc(func() int {
+			return 1
+		}),
+		// Set the callback to call when the slider value is changed
+		widget.SliderOpts.ChangedHandler(func(args *widget.SliderChangedEventArgs) {
+			fmt.Println(args.Current)
+		}),
+	)
+	// Set the current value of the slider
+	slider.Current = 5
+	// add the slider as a child of the container
+	rootContainer.AddChild(slider)
+	// To display the text widget, we have to add it to the root container.
+	rootContainer.AddChild(helloWorldLabel)
+	g.ui = &ebitenui.UI{
+		Container: rootContainer,
 	}
 	return g
 }
@@ -125,6 +206,7 @@ func (g *Game) Update() error {
 			}
 		}
 	}
+	g.ui.Update()
 	return nil
 }
 
@@ -132,11 +214,16 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	for i := 0; i < flyCount; i++ {
 		g.flies[i].Draw(screen)
 	}
+	g.ui.Draw(screen)
 
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return WIN_WIDTH, WIN_HEIGHT
+}
+
+type game struct {
+	ui *ebitenui.UI
 }
 
 func init() {
@@ -146,6 +233,7 @@ func init() {
 func main() {
 	ebiten.SetWindowSize(WIN_WIDTH, WIN_HEIGHT)
 	ebiten.SetWindowTitle("Fireflies Synchronization")
+
 	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
